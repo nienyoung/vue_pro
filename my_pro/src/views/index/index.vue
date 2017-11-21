9c6bf852e00aa8cc9eff0019e2503a932c413f92
nienyoung <iwrotetoyou@sjtu.edu.cn>
2017-11-21 20:52:11 +0800

    first commit
    the project has been packed

---
+++ my_pro/src/views/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h2>欢迎来到图书管理系统</h2>
+		<footer-nav v-bind:class="{'isIndex':isNowPage}"></footer-nav>
+	</div>
+</template>
+
+<script>
+	import FooterNav from '../../components/footer.vue'
+	export default{
+		components: {
+			FooterNav
+		},
+		data(){
+			return{
+				isNowPage: true
+			}
+		}
+	}
+</script>
